80d4ca20021292c2860667b1269159a1bcad4a9f
csekhar193 <chandrasekhar193.ece@gmail.com>
2018-12-11 15:04:12 +0530

    Added token system to app

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ let unifiedServer = function (req, res) {
 //Define a request router
 var router = {
 	'ping' : handlers.ping,
-	'users': handlers.users
+	'users': handlers.users,
+	'tokens': handlers.tokens
 };
 
